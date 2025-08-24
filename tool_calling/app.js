38b828675e310d43e9f00c708354f12eb2c4b380
moizusmancs <moizusmancs@gmail.com>
2025-08-24 23:48:44 +0500

    complete

--- tool_calling/app.js
+++ tool_calling/app.js
@@ -18,91 +18,74 @@ async function main() {
     },
     {
       role: "user",
-      content: "iphone 16 launch date?",
+      content: "current weather today in dera ismail khan",
     },
   ]
 
-  const completion = await groq.chat.completions.create({
-    model: "llama-3.3-70b-versatile",
-    messages: messages,
-      tools: [
-          {
-              type: "function",
-              function: {
-                  name: "webSearch",
-                  description: "search the latest information and realtime data on the internet",
-                  parameters: {
-                      type: "object",
-                      properties: {
-                          query: {
-                              type: "string",
-                              description: "the search query to search for",
-                          }
-                      },
-                      required: ["query"],
-                  },
-              },
-          }
-      ],
-  tool_choice: "auto"
-  });
+  while(true){
 
-  messages.push(completion.choices[0].message)
+    const completion = await groq.chat.completions.create({
+      model: "llama-3.3-70b-versatile",
+      messages: messages,
+      tools: [
+        {
+          type: "function",
+          function: {
+            name: "webSearch",
+            description: "search the latest information and realtime data on the internet",
+            parameters: {
+              type: "object",
+              properties: {
+                query: {
+                  type: "string",
+                  description: "the search query to search for",
+                }
+              },
+              required: ["query"],
+            },
+          },
+        }
+      ],
+      tool_choice: "auto"
+    });
+
+    messages.push(completion.choices[0].message);
+
+    const tool_calls = completion.choices[0].message.tool_calls
+
+    if(!tool_calls){
+      console.log(`Assistant: ${completion.choices[0].message.content}`);
+      break;
+    }
+  
+    for (const tool of tool_calls){
+      // console.log("tool-> ",tool);
+      const functionName = tool.function.name;
+      const functionParams = tool.function.arguments;
+  
+      if (functionName === "webSearch"){
+          const toolResult = await webSearch(JSON.parse(functionParams));
+          // console.log("toolResult: ", toolResult);
+  
+          messages.push({
+            tool_call_id: tool.id,
+            role: "tool",
+            name: functionName,
+            content: toolResult
+          })
+          
+      }    
+    }
+
+  }
+
+  console.log("-------------------------------------------------------");
+  
+  console.log(messages);
+  
+
 
   // console.log(JSON.stringify(completion.choices[0].message,null,2));
-
-  const tool_calls = completion.choices[0].message.tool_calls
-
-  if(!tool_calls){
-    return;
-  }
-
-  for (const tool of tool_calls){
-    console.log("tool-> ",tool);
-    const functionName = tool.function.name;
-    const functionParams = tool.function.arguments;
-
-    if (functionName === "webSearch"){
-        const toolResult = await webSearch(JSON.parse(functionParams));
-        // console.log("toolResult: ", toolResult);
-
-        messages.push({
-          tool_call_id: tool.id,
-          role: "tool",
-          name: functionName,
-          content: toolResult
-        })
-        
-    }    
-  }
-
-  const completion2 = await groq.chat.completions.create({
-    model: "llama-3.3-70b-versatile",
-    messages: messages,
-      tools: [
-          {
-              type: "function",
-              function: {
-                  name: "webSearch",
-                  description: "search the latest information and realtime data on the internet",
-                  parameters: {
-                      type: "object",
-                      properties: {
-                          query: {
-                              type: "string",
-                              description: "the search query to search for",
-                          }
-                      },
-                      required: ["query"],
-                  },
-              },
-          }
-      ],
-  tool_choice: "auto"
-  });
-
-
-  console.log(JSON.stringify(completion2.choices[0].message,null,2));
 
 
   
